ec25c47d679ffc9a1726c1a12f4dfb092c119363
AquilanteJP <54719805+AquilanteJP@users.noreply.github.com>
2023-01-23 19:11:30 -0300

    Create main.cpp

---
+++ MenorMasGrande/main.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <iostream>
+
+using namespace std;
+
+int menorMasGrande(vector<int> v,int x){
+    int res=-1;
+    int i=0;
+    bool flag=false;
+    while(i<v.size() && flag==false){
+        if(v[i]>x){
+            res=i;
+            flag=true;
+        }
+        i++;
+    }
+    return res;
+}
+
+int main() {
+    /* No hace falta modificar el main */
+    // Leo las entradas
+    int n;
+    cin >> n;
+    int i = 0;
+    int x;
+    vector<int> v;// En v leo el vector
+    while (i<n){
+        cin >> x;
+        v.push_back(x);
+        i++;
+    }
+    cin >> x;
+    
+    // Evaluo si el vector v esta ordenado
+    int res = menorMasGrande(v,x);
+    
+    // Imprimo la salida
+    cout << res;
+    
+    return 0;
+}
